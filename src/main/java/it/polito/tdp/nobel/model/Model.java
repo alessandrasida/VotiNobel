a751749e9c1464fb68d8822b6973b72a02ae1379
aless <aless@172.21.43.100>
2023-06-29 14:54:50 +0200

    .

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -19,80 +19,83 @@ public class Model {
 	
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
 		migliore = new HashSet<>();
-		mediaMigliore = 0.0;
+		this.mediaMigliore = 0.0;
 		
-		Set<Esame> parziale = new HashSet<>();  
+		Set<Esame> parziale = new HashSet<>();
 		
 		cercaMeglio(parziale, 0, numeroCrediti);
 		
-		return migliore;		
-
+		return migliore;
 	}
 	
+	
 	private void cercaMeglio(Set<Esame> parziale, int L, int numeroCrediti) {
-		
-		int sommaCrediti = sommaCrediti(parziale);
+int sommaCrediti = this.sommaCrediti(parziale);
 		
 		if (sommaCrediti > numeroCrediti)
 			return;
 		
-		if (sommaCrediti == numeroCrediti) {//potrei avere una soluzione qui
-			double mediaVoti = calcolaMedia(parziale);
-			if (mediaVoti > mediaMigliore) {
+		if( sommaCrediti == numeroCrediti) {//potrei avere una soluzione
+			double mediaVoti = this.calcolaMedia(parziale);
+			if( mediaVoti > mediaMigliore) {
 				mediaMigliore = mediaVoti;
-				migliore = new HashSet<>(parziale);
+				migliore = new HashSet<>(parziale);		
 			}
-			return;
+			return; 		
 		}
-		
-		if (L == allEsami.size())
+			
+		if( L == this.allEsami.size())
 			return;
-		
-		// provo ad aggiungere il prossimo elemento
-		//L=0 {e1} 			  / {}
-		//L=1 {e1, e2} - {e1} / {e2} - {}	
-		
-		parziale.add(allEsami.get(L));
-		cercaMeglio(parziale, L+1, numeroCrediti);
+		//cambiamo il modo in cui aggiungere gli esami
+		//provo ad aggiungere il prox elemento
+		parziale.add(this.allEsami.get(L));
+		this.cercaMeglio(parziale, L+1, numeroCrediti);
 		parziale.remove(allEsami.get(L));
 		
-		// provo a non aggiungere il prossimo elemento
-		cercaMeglio(parziale, L+1, numeroCrediti);	
+		// provo ad aggiungere il prossimo elemento
+				//L=0 {e1} 			  / {}
+				//L=1 {e1, e2} - {e1} / {e2} - {}	
+		this.cercaMeglio(parziale, L+1, numeroCrediti);
+		
 		
 	}
+
 
 	private void cerca(Set<Esame> parziale, int L, int numeroCrediti) {
 		
-		int sommaCrediti = sommaCrediti(parziale);
+		int sommaCrediti = this.sommaCrediti(parziale);
 		
 		if (sommaCrediti > numeroCrediti)
 			return;
 		
-		if (sommaCrediti == numeroCrediti) {//potrei avere una soluzione qui
-			double mediaVoti = calcolaMedia(parziale);
-			if (mediaVoti > mediaMigliore) {
+		if( sommaCrediti == numeroCrediti) {//potrei avere una soluzione
+			double mediaVoti = this.calcolaMedia(parziale);
+			if( mediaVoti > mediaMigliore) {
 				mediaMigliore = mediaVoti;
+				//migliore = parziale; sbagliato!!!!
+				// perché copia solo il riferimento a parziale
+				//se io modifico parziale si modifica anche migliore, quindi devo fare una new
+				//voglio che migliore sia una fotografia di quel che è ora
 				migliore = new HashSet<>(parziale);
+				
 			}
-			return;
+			return; 
+				
 		}
-		
-		if (L == allEsami.size())
-			return;
-		
-		// se arrivo qui, numeroCrediti > sommaCrediti
-		
-		for (Esame e : allEsami) {
 			
-			if (!parziale.contains(e)) {
+		if( L == this.allEsami.size())
+			return;
+		//vuol dire che ho aggiunto tutti gli esami a disposizione, quindi non mi serve andare avanti
+		
+		//se son arrivato fin qui, numeroCrediti> sommaCrediti
+		
+		for( Esame e : this.allEsami) {
+			if( ! parziale.contains(e)) {
 				parziale.add(e);
 				cerca(parziale, L+1, numeroCrediti);
 				parziale.remove(e);
-				
 			}
-			
 		}
-		
 		
 	}
 
